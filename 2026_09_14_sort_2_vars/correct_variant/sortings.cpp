7f72867b932920a62d5993d195ae217f63f45046
Martin <khudyakovmartin@gmail.com>
2026-09-19 17:22:02 +0300

    Implemented sorting function

---
+++ 2026_09_14_sort_2_vars/correct_variant/sortings.cpp
@@ -0,0 +1,12 @@
+#include <utility>
+#include "sortings.hpp"
+
+void biv::my_sort(int* const arr, const int size) {
+	for (int i = 0; i < size - 1; ++i) {
+		for (int j = 0; j < size - 1 - i; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				std::swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
